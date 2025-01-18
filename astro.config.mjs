9da9dd2309baf067bdcf6daba72f2496c9d88ebc
sarkalax <112681783+sarkalax@users.noreply.github.com>
2025-01-18 12:58:20 +0100

    opravy, robot, astro setting, analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://hejbnisebou.cz",
+  integrations: [sitemap()],
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+  build: {
+    inlineStylesheets: "never",
+  },
+});
